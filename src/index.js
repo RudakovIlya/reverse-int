466ac0d5b81d0bfbf400348db995a22c0cda4d14
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-07-31 15:33:25 +0300

    feat: added solution for reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return +n
+        .toString()
+        .split("")
+        .reduceRight((resultValue, value) => {
+            if (isNaN(value)) {
+                return resultValue;
+            }
+            resultValue += value;
+            return resultValue;
+        }, "");
+};
